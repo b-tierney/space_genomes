11fe0dcccc53ebb3eba54f9a075db6e43202051b
b-tierney <braden.tierney@gmail.com>
2021-10-19 09:02:30 -0400

    metapan update

--- gwas_scripts/merge_snp_data.sh
+++ gwas_scripts/merge_snp_data.sh
@@ -7,19 +7,15 @@ ls *typestrain/snps.vcf | xargs vcfcombine > ref-typestrain_snps.vcf
 ls *mt1/snps.vcf | grep -v GCA_013450165 | xargs vcfcombine > ref-mt1_snps.vcf
 ls *otheriss/snps.vcf | xargs vcfcombine > ref-otheriss_snps.vcf
 
-for file in ref*;
+for file in ref-*; do
 
-do
-
-while read p;
-
-	do
-		refsource=$(echo $file | cut -f1 -d_ | cut -f2 -d-)
-		val1=$(echo $p | cut -f1)
-		val2=$(echo $p | cut -f2)
-		sed -i "s/"${val2}"/"${val1}"/g" $file
-		sed -i "s/_"${refsource}"//g" $file
-
-	done<assembly_biosample_mapping
+while read p; 
+do 
+		refsource=$(echo $file | cut -f1 -d_ | cut -f2 -d-) 
+		val1=$(echo $p | cut -f1 | cut -f1 -d' ') 
+		val2=$(echo $p | cut -f2 | cut -f2 -d' ') 
+		sed -i "s/"${val2}"/"${val1}"/g" $file 
+		sed -i "s/_"${refsource}"//g" $file 
+done<mapping.tsv
 
 done
