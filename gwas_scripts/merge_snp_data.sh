e496257f982f1d6896bae5a89407a8b9b28995ed
Braden Tierney <btt4001@mac162548.med.cornell.edu>
2021-10-22 13:22:15 -0400

    pyser update

--- gwas_scripts/merge_snp_data.sh
+++ gwas_scripts/merge_snp_data.sh
@@ -4,11 +4,11 @@
 
 ls *earth/snps.vcf | xargs vcfcombine > ref-earth_snps.vcf
 ls *typestrain/snps.vcf | xargs vcfcombine > ref-typestrain_snps.vcf
-ls *mt1/snps.vcf | grep -v GCA_013450165 | xargs vcfcombine > ref-mt1_snps.vcf
-ls *otheriss/snps.vcf | xargs vcfcombine > ref-otheriss_snps.vcf
+grep -Ff space_genomes_mapped <(ls *mt1/snps.vcf | grep -v GCA_013450165)| xargs vcfcombine > ref-mt1_snps.vcf
+grep -Ff space_genomes_mapped <(ls *otheriss/snps.vcf | grep -v GCA_013450165) | xargs vcfcombine > ref-otheriss_snps.vcf
 
 for file in ref-*; do
-
+echo $file
 while read p; 
 do 
 		refsource=$(echo $file | cut -f1 -d_ | cut -f2 -d-) 
@@ -16,6 +16,12 @@ do
 		val2=$(echo $p | cut -f2 | cut -f2 -d' ') 
 		sed -i "s/"${val2}"/"${val1}"/g" $file 
 		sed -i "s/_"${refsource}"//g" $file 
-done<mapping.tsv
+done<assembly_biosample_mapping
 
 done
+
+bcftools norm -m - <ref-otheriss_snps.vcf> ref-otheriss_snps-norm.vcf
+bcftools norm -m - <ref-mt1_snps.vcf> > ref-mt1_snps-norm.vcf
+bcftools norm -m - <ref-typestrain_snps.vcf> > ref-typestrain_snps-norm.vcf
+bcftools norm -m - <ref-earth_snps.vcf> > ref-earth_snps-norm.vcf
+
--- gwas_scripts/run_pyseer.sh
+++ gwas_scripts/run_pyseer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pyseer --lmm --phenotypes metadata_filtered.tsv --phenotype-column Isolation_Source --kmers output2.txt.gz --similarity distances_apit --output-patterns kmer_patterns_apit_isolation_source.txt --cpu 10 > apit_isolation_source_kmers.txt
+pyseer --lmm --phenotypes pyseer_metadata.tsv --phenotype-column EARTH_SPACE_BINARY --kmers output2.txt.gz --similarity distances_names --output-patterns kmer_patterns_apit_isolation_source.txt --cpu 10 > apit_earth_v_space_kmers.txt &> kmer_earth_space_log
+pyseer --lmm --phenotypes ../pyseer_metadata.tsv --phenotype-column FLIGHT_BINARY --vcf ref-mt1_snps.vcf --similarity ../distances_names --cpu 10 > apit_mt1-mt2_snps.txt &>mt1_mt2_earth_space_log
+pyseer --lmm --phenotypes ../pyseer_metadata.tsv --phenotype-column EARTH_SPACE_BINARY --vcf ref-earth_snps.vcf --similarity ../distances_names  --cpu 10 > apit_earth_v_space_snps.txt &> snp_earth_space_log
+pyseer --lmm --phenotypes ../pyseer_metadata.tsv --phenotype-column EARTH_SPACE_BINARY --vcf ref-typestrain_snps.vcf --similarity ../distances_names --cpu 10 > apit_typestrain_v_space_snps.txt &> typestrain_earth_space_log
 
-pyseer --lmm --phenotypes metadata_filtered.tsv --phenotype-column Project --kmers output2.txt.gz --similarity distances_apit --output-patterns kmer_patterns_apit_isolation_source.txt --cpu 10 > apit_project_kmers.txt
 
