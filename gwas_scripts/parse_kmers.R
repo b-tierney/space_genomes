d8560744c94b50b0b9bdeff14b8c2ba0cc83b1b6
b-tierney <braden.tierney@gmail.com>
2021-10-29 11:07:01 -0400

    update

--- gwas_scripts/parse_kmers.R
+++ gwas_scripts/parse_kmers.R
@@ -3,18 +3,32 @@
 library(tidyverse)
 
 data = read.table('apit_earth_v_space_kmers.txt',sep='\t',header=T)
-data = data %>% mutate(by = p.adjust(lrt-pvalue,method='BY')) %>% filter(by <0.05)
+data_sub = data %>% mutate(by = p.adjust(lrt.pvalue,method='BY')) %>% filter(by <0.05)
+saveRDS(data_sub,'apit_earth_v_space_kmers_sig.rds')
 
-data %>% select(XXX) %>% unlist %>% unname
+seqs_pos = data_sub %>% filter(beta>=0) %>% select(variant) %>% unlist %>% unname
+seqs_neg = data_sub %>% filter(beta<=0)%>% select(variant) %>% unlist %>% unname
 
 output = list()
 count = 0
-for(seq in data){
+for(seq in seqs_pos){
 	count = count + 1
 	output[[count]] = paste('>kmer_',count,sep='')
 	count = count + 1
 	output[[count]] = seq
 }
 
-output = output %>% as.data.frame
-write.table(output,row.names=F,header=F)
+output = output %>% as.data.frame %>% t
+write.table(output,'apit_earth_v_space_kmers_sig_seqs_pos.fa',row.names=F,col.names=F,quote=F)
+
+output = list()
+count = 0
+for(seq in seqs_neg){
+	count = count + 1
+	output[[count]] = paste('>kmer_',count,sep='')
+	count = count + 1
+	output[[count]] = seq
+}
+
+output = output %>% as.data.frame %>% t
+write.table(output,'apit_earth_v_space_kmers_sig_seqs_neg.fa',row.names=F,col.names=F,quote=F)
